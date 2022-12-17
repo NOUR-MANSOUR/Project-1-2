9902904528537c91410c8724ec60a8bc47b494de
Nour Mansour <112758883+NOUR-MANSOUR@users.noreply.github.com>
2022-12-17 22:35:33 +0200

    Add files via upload

---
+++ Prime Numbers/Prime Numbers.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please Enter Start Number: ");
+        int start = int.Parse(Console.ReadLine());
+        Console.WriteLine("Please Enter End Number: ");
+        int end = int.Parse(Console.ReadLine());
+        Console.WriteLine($"The Prime Numbers From {start} To {end} Is: ");
+       
+        for ( ; start <= end ; start++) {
+            int x = 0;
+            if (start == 1 || start == 0)
+            {
+                continue;
+            }
+            for (int i = 2; i < start; i++)
+            { 
+             
+             if ( start % i == 0)
+                {
+                    x = 1; 
+                    break;
+                }
+            }
+            if (x != 1 )
+            {
+                Console.WriteLine(start);
+            }
+        }
+    }
+}
